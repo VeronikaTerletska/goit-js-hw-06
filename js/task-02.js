930374339b31ea9b33fe995a3968e856ebb75a48
VeronikaTerletska <104487544+VeronikaTerletska@users.noreply.github.com>
2022-09-09 22:15:08 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsList = document.querySelector("#ingredients");
+
+const makeItemsList = items => {
+  return items.map(items => {
+    const itemsEl = document.createElement("li");
+    itemsEl.classList.add("item");
+    itemsEl.textContent = `${items}`;
+    return itemsEl;
+  });
+};
+const elements = makeItemsList(ingredients);
+ingredientsList.append(...elements);
