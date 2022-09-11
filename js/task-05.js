6506911ba506cdaf9895eef17238cd42340c2441
VeronikaTerletska <104487544+VeronikaTerletska@users.noreply.github.com>
2022-09-11 16:53:38 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,6 @@ const inputRef = {
   input: document.querySelector("#name-input"),
   nameLabel: document.querySelector("#name-output"),
 };
-inputRef.input.addEventListener("input", onInputChange);
-function onInputChange(event) {
+inputRef.input.addEventListener("input", event => {
   inputRef.nameLabel.textContent = event.currentTarget.value || "Anonymous";
-}
+});
