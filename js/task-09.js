ff4181c49608dff3ef568a212d0e10d9481eb680
VeronikaTerletska <104487544+VeronikaTerletska@users.noreply.github.com>
2022-09-11 16:53:52 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  colorEl.textContent = `${randomColor}`;
+  document.body.style.backgroundColor = randomColor;
+});
